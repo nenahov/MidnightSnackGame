442f168829c5730f193ef516c16f1af501ee06e0
Vladimir Nenakhov <vladimir_n@crystals.ru>
2025-01-26 02:09:30 +0300

    add Class Docstrings

--- domain/person.py
+++ domain/person.py
@@ -1,5 +1,11 @@
 # Класс персонажа, у которого есть следующие поля: имя, пол, наличие предметов в инвентаре, здоровье, цель, текущее местоположение
 class Person:
+    """
+    Класс игрового персонажа
+
+    У него есть имя, текущее местоположение, предметы в инвентаре, цель и другие показатели.
+    """
+
     def __init__(self, name: str, gender: str, inventory: set, health: int, goal: str, location: str):
         self.name = name
         self.gender = gender
@@ -13,4 +19,3 @@ class Person:
 
     def __repr__(self):
         return f"Name: {self.name}, Gender: {self.gender}, Inventory: {self.inventory}, Health: {self.health}, Goal: {self.goal}, Location: {self.location}"
-
